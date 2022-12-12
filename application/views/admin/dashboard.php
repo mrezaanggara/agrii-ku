a074d047d288e312b45d345b6062929aa4faeb60
Muhammad Reza Anggara <38530689+muhammadreza1044@users.noreply.github.com>
2022-12-12 21:26:02 +0700

    update dashboard admin

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -22,7 +22,7 @@
 
                 <!--content -->
                 <div class="container-fluid">
-                    <h1 class="h4 mb-4 text-gray-800">Selamat datang, ...... di halaman admin Agriiku</h2>
+                    <h1 class="h4 mb-4 text-gray-800">Selamat datang, <?php echo $this->session->userdata('nama') ?>.. <br> Di halaman admin Agriiku</h2>
                 </div>
 
             </div>
